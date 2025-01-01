dc4c7f2ae9beb5f6aa6ff567d18ceb1ddbc606cd
William Oliveira Silva <williamsilva20062005@gmail.com>
2025-01-01 19:05:15 -0300

    Implement product creation and update form with improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,72 @@ export default function Home() {
   const [produtos, setProdutos] = useState<any>([]);
   const [produto, setProduto] = useState<any>({});
 
+  async function criarProduto() {
+    await fetch("http://localhost:3001/produtos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(produto),
+    });
+    setProduto({});
+    await obterProdutos();
+  }
+
+  async function obterProdutos() {
+    const resposta = await fetch("http://localhost:3001/produtos");
+    const produtos = await resposta.json();
+    setProdutos(produtos);
+  }
+
   function renderizarFormProdutos() {
     return (
-      <form className="flex flex-col gap-2">
-        <input
-          type="text"
-          placeholder="Nome"
-          value={produto.nome}
-          onChange={(e) => ({ ...produto, nome: e.target.value })}
-          className="bg-zinc-700 p-2 rounded"
-        />
-        <input type="text" placeholder="Preço" />
-        <button>Adicionar</button>
+      <form className="flex gap-5">
+        <div className="flex flex-col ">
+          <label htmlFor="nome">Nome</label>
+          <input
+            id="nome"
+            type="text"
+            placeholder="Nome"
+            value={produto.nome}
+            onChange={(e) => ({ ...produto, nome: e.target.value })}
+            className="bg-zinc-700 p-2 rounded"
+          />
+        </div>
+
+        <div className="flex flex-col ">
+          <label htmlFor="descricao">Descrição</label>
+          <input
+            id="descricao"
+            type="text"
+            placeholder="Descrição"
+            value={produto.descricao}
+            onChange={(e) => ({ ...produto, descricao: e.target.value })}
+            className="bg-zinc-700 p-2 rounded"
+          />
+        </div>
+
+        <div className="flex flex-col ">
+          <label htmlFor="preco">Preço</label>
+          <input
+            id="preco"
+            type="number"
+            placeholder="Preço"
+            value={produto.preco}
+            onChange={(e) => ({ ...produto, preco: e.target.value })}
+            className="bg-zinc-700 p-2 rounded"
+          />
+        </div>
+        <div>
+          <button
+            className="bg-blue-500 p-2 rounded-md"
+            onClick={() => {
+              criarProduto();
+            }}
+          >
+            Criar Produto
+          </button>
+        </div>
       </form>
     );
   }
@@ -32,11 +86,6 @@ export default function Home() {
         ))}
       </div>
     );
-  }
-  async function obterProdutos() {
-    const resposta = await fetch("http://localhost:3001/produtos");
-    const produtos = await resposta.json();
-    setProdutos(produtos);
   }
 
   useEffect(() => {
