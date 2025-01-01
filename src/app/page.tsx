ff59d87c0143db820f615dd5898b49fda4686324
William Oliveira Silva <williamsilva20062005@gmail.com>
2025-01-01 19:33:35 -0300

    Refactor product management functions and improve form layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 "use client";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [produtos, setProdutos] = useState<any>([]);
   const [produto, setProduto] = useState<any>({});
+  const [produtos, setProdutos] = useState<any>([]);
+
+  useEffect(() => {
+    obterProdutos();
+  }, []);
+
+  async function obterProdutos() {
+    const resp = await fetch("http://localhost:3001/produtos");
+    const produtos = await resp.json();
+    setProdutos(produtos);
+  }
 
   async function criarProduto() {
     await fetch("http://localhost:3001/produtos", {
@@ -17,84 +27,120 @@ export default function Home() {
     await obterProdutos();
   }
 
-  async function obterProdutos() {
-    const resposta = await fetch("http://localhost:3001/produtos");
-    const produtos = await resposta.json();
-    setProdutos(produtos);
+  async function alterarProduto() {
+    await fetch(`http://localhost:3001/produtos/${produto.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(produto),
+    });
+    setProduto({});
+    await obterProdutos();
   }
 
-  function renderizarFormProdutos() {
+  async function obterProdutoPorId(id: number) {
+    const resp = await fetch(`http://localhost:3001/produtos/${id}`);
+    const produto = await resp.json();
+    setProduto(produto);
+  }
+
+  async function excluirProduto(id: number) {
+    await fetch(`http://localhost:3001/produtos/${id}`, {
+      method: "DELETE",
+    });
+    await obterProdutos();
+  }
+
+  function renderizarFormProduto() {
     return (
-      <form className="flex gap-5">
-        <div className="flex flex-col ">
+      <div className="flex gap-5 items-end">
+        <div className="flex flex-col">
           <label htmlFor="nome">Nome</label>
           <input
             id="nome"
             type="text"
-            placeholder="Nome"
-            value={produto.nome}
-            onChange={(e) => ({ ...produto, nome: e.target.value })}
-            className="bg-zinc-700 p-2 rounded"
+            value={produto.nome ?? ""}
+            onChange={(e) => setProduto({ ...produto, nome: e.target.value })}
+            className="bg-zinc-700 p-2 rounded-md"
           />
         </div>
-
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
           <label htmlFor="descricao">Descrição</label>
           <input
             id="descricao"
             type="text"
-            placeholder="Descrição"
-            value={produto.descricao}
-            onChange={(e) => ({ ...produto, descricao: e.target.value })}
-            className="bg-zinc-700 p-2 rounded"
+            value={produto.descricao ?? ""}
+            onChange={(e) =>
+              setProduto({ ...produto, descricao: e.target.value })
+            }
+            className="bg-zinc-700 p-2 rounded-md"
           />
         </div>
-
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
           <label htmlFor="preco">Preço</label>
           <input
             id="preco"
             type="number"
-            placeholder="Preço"
-            value={produto.preco}
-            onChange={(e) => ({ ...produto, preco: e.target.value })}
-            className="bg-zinc-700 p-2 rounded"
+            value={produto.preco ?? ""}
+            onChange={(e) => setProduto({ ...produto, preco: +e.target.value })}
+            className="bg-zinc-700 p-2 rounded-md"
           />
         </div>
         <div>
-          <button
-            className="bg-blue-500 p-2 rounded-md"
-            onClick={() => {
-              criarProduto();
-            }}
-          >
-            Criar Produto
-          </button>
+          {produto.id ? (
+            <button
+              onClick={alterarProduto}
+              className="bg-blue-500 px-4 py-2 rounded-md"
+            >
+              Alterar Produto
+            </button>
+          ) : (
+            <button
+              onClick={criarProduto}
+              className="bg-blue-500 px-4 py-2 rounded-md"
+            >
+              Criar Produto
+            </button>
+          )}
         </div>
-      </form>
+      </div>
     );
   }
 
   function renderizarProdutos() {
     return (
       <div className="flex flex-col gap-2">
-        {produtos.map((produto) => (
-          <div key={produto.id} className="flex gap-2 bg-zinc-800 p-2 rounded">
-            <h1>{produto.nome}</h1>
-            <p>{produto.preco}</p>
+        {produtos.map((produto: any) => (
+          <div
+            key={produto.id}
+            className="flex items-center gap-2 bg-zinc-800 px-4 py-2 rounded-md"
+          >
+            <div className="flex-1">{produto.nome}</div>
+            <div>{produto.preco}</div>
+            <div>
+              <button
+                onClick={() => obterProdutoPorId(produto.id)}
+                className="bg-green-500 p-2 rounded-md"
+              >
+                Alterar
+              </button>
+              <button
+                onClick={() => excluirProduto(produto.id)}
+                className="bg-red-500 p-2 rounded-md"
+              >
+                Excluir
+              </button>
+            </div>
           </div>
         ))}
       </div>
     );
   }
 
-  useEffect(() => {
-    obterProdutos();
-  }, []);
-
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-2">
-      {renderizarFormProdutos()}
+    <div className="flex flex-col justify-center items-center h-screen gap-10">
+      {renderizarFormProduto()}
       {renderizarProdutos()}
     </div>
   );
